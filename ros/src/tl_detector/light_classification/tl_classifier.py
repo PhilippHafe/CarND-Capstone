a7ea652f67f601f09abb0fbc6433dd31d8fa82a6
PhilippHafe <philipp.hafemann@rwth-aachen.de>
2020-08-27 16:01:42 +0200

    classifier v1

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,29 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
+import datetime
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        PATH_TO_CKPT = "light_classification/models/frozen_interference_graph.pb"
+        self.graph = tf.Graph()
+        self.threshold = 0.5
+
+        with self.graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)        
+                tf.import_graph_def(od_graph_def, name = '')
+
+            self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+            self.boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+            self.scores = self.graph.get_tensor_by_name('detection_scores:0')
+            self.classes = self.graph.get_tensor_by_name('detection_classes:0')
+            self.num_detections = self.graph.get_tensor_by_name('num_detections:0')
+
+        self.sess = tf.Session(graph=self.graph)
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +36,32 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
+        with self.graph.as_default():
+            image_np_expanded = np.expand_dims(image, axis=0)
+            start = datetime.datetime.now()
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.boxes, self.scores, self.classes, self.num_detections],
+                feed_dict={self.image_tensor: image_np_expanded})
+            end = datetime.datetime.now()
+            dur = end - start
+            print(dur.total_seconds())
+
+        boxes = np.squeeze(boxes)
+        classes = np.squeeze(classes).astype(np.int32)
+        scores = np.squeeze(scores)
+        
+        print('Score: ', scores[0])
+        print('Classes: ', classes[0])
+
+        if scores[0] > self.threshold:
+            if classes[0] == 1:
+                print('Green')
+                return TrafficLight.GREEN
+            elif classes[0] == 2:
+                print('Red')
+                return TrafficLight.RED
+            elif classes[0] == 3:
+                print('Yellow')
+                return TrafficLight.YELLOW
+
         return TrafficLight.UNKNOWN
